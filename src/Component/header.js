c4ae4ab2b0cbd32149372f3770bba27a7a7aa30c
edolubis21 <edolubis12345@gmail.com>
2020-04-08 15:26:55 +0700

    TDS task

---
+++ src/Component/header.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import "../Style/style.scss";
+
+class Header extends Component {
+  render() {
+    return (
+      <header>
+        <p>header</p>
+      </header>
+    );
+  }
+}
+
+export default Header;
---
+++ src/Screen/home.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import Header from "../Component/header";
+
+class Home extends Component {
+  render() {
+    return (
+      <div>
+        <Header />
+        <p>halaman 1</p>
+      </div>
+    );
+  }
+}
+
+export default Home;
